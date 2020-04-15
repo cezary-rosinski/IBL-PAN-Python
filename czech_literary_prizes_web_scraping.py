963ced685bfeb111ce99b36bb37450cf56b72cae
cezary-rosinski <cezary.rosinski@gmail.com>
2020-04-15 12:26:30 +0200

    15.04.2020;home

---
+++ czech_literary_prizes_web_scraping.py
@@ -0,0 +1,44 @@
+import time
+start_time = time.time()
+import requests
+from bs4 import BeautifulSoup
+import re
+
+FormData = {'q' : '*',
+            'so' : 've jménech osob'}
+r = requests.post("http://ceny.ucl.cas.cz/index.php", data=FormData)
+r.encoding = 'ISO 8859-2'
+tekst = r.text
+soup = BeautifulSoup(tekst, 'html.parser')
+links = soup.findAll('a', attrs={'href': re.compile("^\?o\=")})
+
+urls = []
+for link in links:
+    urls.append([link.text, "http://ceny.ucl.cas.cz/index.php" + link['href']])
+
+data= []
+for i, url in enumerate(urls, 1):
+    if len(url[0]) > 0:
+        print(str(i) + '/' + str(len(urls)))
+        page = requests.get(url[1])
+        page.encoding = 'ISO 8859-2'
+        tree = BeautifulSoup(page.text, 'html.parser')
+        try:
+            data_person = tree.select_one('h2').text
+        except:
+            data_person = "brak danych (CR)"
+        try:
+            role = tree.select_one('h3').text
+        except:
+            role = "brak danych (CR)"
+        try:
+            description = tree.select_one('.indent').text
+        except:
+            description = "brak danych (CR)"
+        data.append([data_person, role, description])
+end_time = time.time()
+print(end_time - start_time)
+
+import pandas as pd
+
+df = pd.DataFrame(data, columns =['data_person', 'role', 'description'])
