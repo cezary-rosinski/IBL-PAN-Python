9c9845099a710b6dcc135760fca1ff579602a48d
Cezary <cezary.rosinski@gmail.com>
2020-05-23 23:25:16 +0200

    23.05.2020; machine

--- bn_marc_to_table.py
+++ bn_marc_to_table.py
@@ -37,6 +37,7 @@ for i, file_path in enumerate(files):
         df = df.drop_duplicates().reset_index(drop=True)
         marc_df = marc_df.append(df)
 
+marc_df = marc_df.drop_duplicates().reset_index(drop=True)
 marc_df_wide = marc_df.pivot(index = 'id', columns = 'field', values = 'content')
 marc_df_wide.to_excel('BN_books_2013-2019.xlsx', index=False)
 
