3434b3d6839c50a8d58742ed67566f1a80efcc9f
Cezary <cezary.rosinski@gmail.com>
2020-09-01 15:22:15 +0200

    01.09.2020; home

--- BN_MAK_journals_web_scraping.py
+++ BN_MAK_journals_web_scraping.py
@@ -4,6 +4,8 @@ import re
 from selenium.common.exceptions import NoSuchElementException
 import pandas as pd
 import unidecode
+import regex
+import numpy as np
 
 def diacritics_unidecode_to_lower(x):
     english_alphabet = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split(' ')
@@ -15,8 +17,18 @@ def diacritics_unidecode_to_lower(x):
             new_word += unidecode.unidecode(letter.lower())
     return new_word.replace(' ', 'b')
 
+def get_year(x):
+    try: 
+        val = [e for e in x if e.startswith('adres')][0]
+        val = re.findall('\d{4}', val)[-1]
+    except IndexError:
+        val = np.nan
+    return val
+
 browser = webdriver.Chrome()
 browser.get("http://mak.bn.org.pl/cgi-bin/makwww.exe?BM=24&IZ=Has%B3o_przedm")
+# browser.get("http://mak.bn.org.pl/cgi-bin/makwww.exe?BM=24&IM=04&TX=&PD=dalej&WI=JeZYKIbBAlTYCKIE")
+# browser.get("http://mak.bn.org.pl/cgi-bin/makwww.exe?BM=24&IM=04&TX=&PD=dalej&WI=WYCENAbNIERUCHOMOsCI")
 list_of_journals = []
 next_20 = True
 while next_20:
@@ -65,8 +77,15 @@ while next_20:
         next_20 = True
     except NoSuchElementException:
         next_20 = False
+        
+list_of_journals_unique = list(set(list_of_journals))
 
-df = pd.DataFrame(list_of_journals, columns = ['hasło przedmiotowe', 'body'])
+df = pd.DataFrame(list_of_journals_unique, columns = ['hasło przedmiotowe', 'body']).sort_values('hasło przedmiotowe')
+df['body'] = df['body'].str.split('\n')
+df['title'] = df['body'].apply(lambda x: x[0]).apply(lambda x: x[[match.start() for match in regex.finditer("\p{Lu}", x)][0]:]).str.split(' / ', 1)
+df['institution'] = df['title'].apply(lambda x: x[-1])
+df['title'] = df['title'].apply(lambda x: x[0])
+df['since'] = df['body'].apply(lambda x: get_year(x))
 
 df.to_excel('BN_MAK_journals.xlsx', index=False)
 
