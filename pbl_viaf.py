d22adbfc70821fcdbf1022eb1a3f21bb5b27023f
Cezary <cezary.rosinski@gmail.com>
2020-11-17 15:17:48 +0100

    17.11.2020; home

--- pbl_viaf.py
+++ pbl_viaf.py
@@ -6,13 +6,13 @@ import numpy as np
 import copy
 import math
 from datetime import datetime
-import re
+import regex as re
 from functools import reduce
 import pandasql
 from urllib.parse import urlparse
 import requests
 from bs4 import BeautifulSoup
-from Levenshtein import distance as levenshtein_distance
+#from Levenshtein import distance as levenshtein_distance
 
 
 dsn_tns = cx_Oracle.makedsn('pbl.ibl.poznan.pl', '1521', service_name='xe')
@@ -21,72 +21,60 @@ connection = cx_Oracle.connect(user='IBL_SELECT', password='CR333444', dsn=dsn_t
 # PBL queries
 
 pbl_indeks_osobowy = """select distinct ind.odi_imie, ind.odi_nazwisko
-from IBL_OWNER.pbl_osoby_do_indeksu ind"""
-                    
+from IBL_OWNER.pbl_osoby_do_indeksu ind"""                   
 pbl_indeks_osobowy = pd.read_sql(pbl_indeks_osobowy, con=connection).fillna(value = np.nan)
-pbl_indeks_osobowy['full name'] = pbl_indeks_osobowy['ODI_NAZWISKO'] + ' ' + pbl_indeks_osobowy['ODI_IMIE']
+pbl_indeks_osobowy['full name'] = pbl_indeks_osobowy.apply(lambda x: ' '.join(x.dropna().astype(str)), axis=1)
 
-test = pbl_indeks_osobowy.head(100)
+cluster_maryi = pbl_indeks_osobowy[pbl_indeks_osobowy['ODI_NAZWISKO'].str.contains('Jezus')]
+
+test = pbl_indeks_osobowy.head(25)
 # viaf
 viaf_errors = []
 pbl_indeks_viaf = pd.DataFrame()
 for index, row in test.iterrows():
-    print(str(index) + '/' + str(len(test)))
+    print(str(index+1) + '/' + str(len(test)))
     try:
         url = re.sub('\s+', '%20', f"http://viaf.org/viaf/search?query=local.personalNames%20all%20%22{row['full name']}%22&sortKeys=holdingscount&recordSchema=BriefVIAF")
-        url = 'http://viaf.org/viaf/search?query=local.personalNames%20all%20%22Uspie%C5%84ski%20Edward%22&sortKeys=holdingscount&recordSchema=BriefVIAF'
-        index = 50
-        row = test.iloc[50]
         response = requests.get(url)
         response.encoding = 'UTF-8'
         soup = BeautifulSoup(response.text, 'html.parser')
         people_links = soup.findAll('a', attrs={'href': re.compile("viaf/\d+")})
-        flags = people_links.find_all('img', attrs={'src': re.compile("viaf/images/flags")})
         viaf_people = []
         for people in people_links:
-            print(people.text)
-            flags = people.find_all('img', attrs={'src': re.compile("viaf/images/flags")})
-            for flag in flags:
-                print(flag['title'])
-            print('______________________')
-            
             person_name = re.split('â\x80\x8e|\u200e ', re.sub('\s+', ' ', people.text).strip())
             person_link = re.sub(r'(.+?)(\#.+$)', r'http://viaf.org\1viaf.xml', people['href'].strip())
-            viaf_people.append([person_name, person_link])
-        viaf_people = pd.DataFrame(viaf_people, columns=['viaf name', 'viaf'])
-        viaf_people = pd.DataFrame(viaf_people['viaf name'].tolist(), viaf_people['viaf']).stack()
-        viaf_people = viaf_people.reset_index()[[0, 'viaf']]
-        viaf_people.columns = ['viaf name', 'viaf']
+            person_link = [person_link] * len(person_name)
+            libraries = str(people).split('<br/>')
+            libraries = [re.sub('(.+)(\<span.*+$)', r'\2', s.replace('\n', ' ')) for s in libraries if 'span' in s]
+            single_record = list(zip(person_name, person_link, libraries))
+            viaf_people += single_record
+        viaf_people = pd.DataFrame(viaf_people, columns=['viaf name', 'viaf', 'libraries'])
         viaf_people['full name'] = f"{row['full name']}"
         for ind, vname in viaf_people.iterrows():
             viaf_people.at[ind, 'cosine'] = get_cosine_result(vname['viaf name'], vname['full name'])
-        if viaf_people['cosine'].max() == 0:
-            for ind, vname in viaf_people.iterrows():
-                viaf_people.at[ind, 'levenshtein'] = levenshtein_distance(vname['viaf name'], vname['full name'])
-            
-            
-        
         viaf_people['viaf len'] = viaf_people['viaf'].apply(lambda x: len(x))
-        viaf_people = viaf_people[(viaf_people['cosine'] == viaf_people['cosine'].max()) &
-                                  (viaf_people['viaf len'] == viaf_people['viaf len'].min())]
 
-# zamiast head dać polską nazwę, jeśli nie ma to head
-        
-        viaf_people = viaf_people.head(1).drop(columns=['cosine', 'viaf len'])
+        viaf_people = viaf_people[viaf_people['cosine'] == viaf_people['cosine'].max()]
+        viaf_people = viaf_people[viaf_people['viaf len'] == viaf_people['viaf len'].min()]
+        if len(viaf_people[viaf_people['libraries'].str.contains('Biblioteka Narodowa (Polska)|NUKAT (Polska)')]) == 0:
+            viaf_people = viaf_people.head(1).drop(columns=['cosine', 'viaf len', 'libraries'])
+        else:
+            viaf_people = viaf_people[viaf_people['libraries'].str.contains('Biblioteka Narodowa (Polska)|NUKAT (Polska)')]
         pbl_indeks_viaf = pbl_indeks_viaf.append(viaf_people)
-    except IndexError:
+    except (IndexError, KeyError):
         error = [row['full name']]
         viaf_errors.append(error)   
         
-test = pd.merge(test, viaf_people, how='left', on='full name')
+test = pd.merge(test, pbl_indeks_viaf, how='left', on='full name')
       
-viaf_df = pd.DataFrame(viaf_enrichment, columns=['index', 'cz_name', 'cz_dates', 'viaf_id', 'IDs', 'nationality', 'occupation', 'language', 'name_and_source'])
-errorfile = io.open('cz_translation_errors.txt', 'wt', encoding = 'UTF-8')
-for element in viaf_errors:
-errorfile.write(str(element) + '\n\n')
-errorfile.close()
 
-viaf_df.to_excel('cz_viaf.xlsx', index = False)
+
+
+
+
+
+
+
 
 
 
