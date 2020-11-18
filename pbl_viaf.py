89eff65243b1ab90ca259c4e0773e05843b63ed6
Cezary <cezary.rosinski@gmail.com>
2020-11-18 13:42:24 +0100

    18.11.2020; machine

--- pbl_viaf.py
+++ pbl_viaf.py
@@ -6,6 +6,7 @@ import regex as re
 import requests
 from bs4 import BeautifulSoup
 import itertools
+import time
 
 dsn_tns = cx_Oracle.makedsn('pbl.ibl.poznan.pl', '1521', service_name='xe')
 connection = cx_Oracle.connect(user='IBL_SELECT', password='CR333444', dsn=dsn_tns, encoding='windows-1250')
@@ -18,43 +19,52 @@ pbl_indeks_osobowy = pd.read_sql(pbl_indeks_osobowy, con=connection).fillna(valu
 
 pbl_indeks_osobowy['full name'] = pbl_indeks_osobowy.apply(lambda x: ' '.join(x.dropna().astype(str)), axis=1).str.replace('\* ', '').str.replace(' \*', '')
 
-pbl_indeks_osobowy.to_excel('pbl_indeks_osobowy.xlsx', index=False)
-
-pbl_indeks_osobowy = pd.read_excel('pbl_indeks_osobowy.xlsx')
+# =============================================================================
+# pbl_indeks_osobowy.to_excel('pbl_indeks_osobowy.xlsx', index=False)
+# pbl_indeks_osobowy = pd.read_excel('pbl_indeks_osobowy.xlsx')
+# =============================================================================
 
 # viaf
 pbl_indeks_viaf = pd.DataFrame()
 for index, row in pbl_indeks_osobowy.iterrows():
     print(str(index+1) + '/' + str(len(pbl_indeks_osobowy)))
-    try:
-        url = re.sub('\s+', '%20', f"http://viaf.org/viaf/search?query=local.personalNames%20all%20%22{row['full name']}%22&sortKeys=holdingscount&recordSchema=BriefVIAF")
-        response = requests.get(url)
-        response.encoding = 'UTF-8'
-        soup = BeautifulSoup(response.text, 'html.parser')
-        people_links = soup.findAll('a', attrs={'href': re.compile("viaf/\d+")})
-        viaf_people = []
-        for people in people_links:
-            person_name = re.split('â\x80\x8e|\u200e ', re.sub('\s+', ' ', people.text).strip())
-            person_link = re.sub(r'(.+?)(\#.+$)', r'http://viaf.org\1viaf.xml', people['href'].strip())
-            person_link = [person_link] * len(person_name)
-            libraries = str(people).split('<br/>')
-            libraries = [re.sub('(.+)(\<span.*+$)', r'\2', s.replace('\n', ' ')) for s in libraries if 'span' in s]
-            single_record = list(zip(person_name, person_link, libraries))
-            viaf_people += single_record
-        viaf_people = pd.DataFrame(viaf_people, columns=['viaf name', 'viaf', 'libraries'])
-        viaf_people['full name'] = f"{row['full name']}"
-        for ind, vname in viaf_people.iterrows():
-            viaf_people.at[ind, 'cosine'] = get_cosine_result(vname['viaf name'], vname['full name'])
-
-        viaf_people = viaf_people[viaf_people['cosine'] == viaf_people['cosine'].max()]
-        if len(viaf_people[viaf_people['libraries'].str.contains('Biblioteka Narodowa \(Polska\)|NUKAT \(Polska\)|National Library of Poland|NUKAT Center of Warsaw University Library')]) == 0:
-            viaf_people = viaf_people.head(1).drop(columns=['cosine', 'libraries'])
-        else:
-            viaf_people = viaf_people[viaf_people['libraries'].str.contains('Biblioteka Narodowa \(Polska\)|NUKAT \(Polska\)|National Library of Poland|NUKAT Center of Warsaw University Library')]
-            viaf_people = viaf_people.head(1).drop(columns=['cosine', 'libraries'])
-        pbl_indeks_viaf = pbl_indeks_viaf.append(viaf_people)
-    except (IndexError, KeyError):
-        pass
+    connection_no = 1
+    while True:
+        try:
+            url = re.sub('\s+', '%20', f"http://viaf.org/viaf/search?query=local.personalNames%20all%20%22{row['full name']}%22&sortKeys=holdingscount&recordSchema=BriefVIAF")
+            response = requests.get(url)
+            response.encoding = 'UTF-8'
+            soup = BeautifulSoup(response.text, 'html.parser')
+            people_links = soup.findAll('a', attrs={'href': re.compile("viaf/\d+")})
+            viaf_people = []
+            for people in people_links:
+                person_name = re.split('â\x80\x8e|\u200e ', re.sub('\s+', ' ', people.text).strip())
+                person_link = re.sub(r'(.+?)(\#.+$)', r'http://viaf.org\1viaf.xml', people['href'].strip())
+                person_link = [person_link] * len(person_name)
+                libraries = str(people).split('<br/>')
+                libraries = [re.sub('(.+)(\<span.*+$)', r'\2', s.replace('\n', ' ')) for s in libraries if 'span' in s]
+                single_record = list(zip(person_name, person_link, libraries))
+                viaf_people += single_record
+            viaf_people = pd.DataFrame(viaf_people, columns=['viaf name', 'viaf', 'libraries'])
+            viaf_people['full name'] = f"{row['full name']}"
+            for ind, vname in viaf_people.iterrows():
+                viaf_people.at[ind, 'cosine'] = get_cosine_result(vname['viaf name'], vname['full name'])
+    
+            viaf_people = viaf_people[viaf_people['cosine'] == viaf_people['cosine'].max()]
+            if len(viaf_people[viaf_people['libraries'].str.contains('Biblioteka Narodowa \(Polska\)|NUKAT \(Polska\)|National Library of Poland|NUKAT Center of Warsaw University Library')]) == 0:
+                viaf_people = viaf_people.head(1).drop(columns=['cosine', 'libraries'])
+            else:
+                viaf_people = viaf_people[viaf_people['libraries'].str.contains('Biblioteka Narodowa \(Polska\)|NUKAT \(Polska\)|National Library of Poland|NUKAT Center of Warsaw University Library')]
+                viaf_people = viaf_people.head(1).drop(columns=['cosine', 'libraries'])
+            pbl_indeks_viaf = pbl_indeks_viaf.append(viaf_people)
+        except (IndexError, KeyError):
+            pass
+        except ConnectionError:
+            print(connection_no)
+            connection_no += 1
+            time.sleep(300)
+            continue
+        break
         
 pbl_indeks_osobowy = pd.merge(pbl_indeks_osobowy, pbl_indeks_viaf, how='left', on='full name')
 pbl_indeks_osobowy['viaf name'] = pbl_indeks_osobowy['viaf name'].str.replace('\\u200e', '').str.strip()
