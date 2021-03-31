c74ac75b84529485f19fae4e14868ebf22ac4840
Cezary <cezary.rosinski@gmail.com>
2021-03-31 17:23:02 +0200

    31.03.2021; home

--- libri_project_descriptors_mapping.py
+++ libri_project_descriptors_mapping.py
@@ -41,9 +41,8 @@ file_list = drive.ListFile({'q': "'0B0l0pB6Tt9olWlJVcDFZQ010R0E' in parents and 
 file_list = drive.ListFile({'q': "'1tPr_Ly9Lf0ZwgRQjj_iNVt-T15FSWbId' in parents and trashed=false"}).GetList()
 file_list = drive.ListFile({'q': "'1xzqGIfZllmXXTh2dJABeHbRPFAM34nbw' in parents and trashed=false"}).GetList()
 #[print(e['title'], e['id']) for e in file_list]
-mapping_files = [file['id'] for file in file_list if file['title'].startswith('mapowanie BN-Oracle')]
-mapping_files_650 = mapping_files[:-1]
-mapping_files_655 = mapping_files[-1]
+mapping_files_655 = [file['id'] for file in file_list if file['title'] == 'mapowanie BN-Oracle - 655'][0]
+mapping_files_650 = [file['id'] for file in file_list if file['title'].startswith('mapowanie BN-Oracle') if file['id'] != mapping_files_655]
 
 #%% dictionary processing
 df_650 = pd.DataFrame()
@@ -66,7 +65,9 @@ for i, row in tqdm(df_650.iterrows(), total=df_650.shape[0]):
     lista_hasel = [e for e in lista_hasel if pd.notnull(e)]
     dict_650[row['X650']] = {'decyzja': row['decyzja'], 'działy PBL': lista_dzialow, 'hasła PBL': lista_hasel}
 
-df_655 = df_655[df_655['decyzja'].isin(['margines', 'zmapowane'])][['X655', 'decyzja', 'dzial_PBL_1', 'dzial_PBL_2', 'dzial_PBL_3', 'dzial_PBL_4', 'dzial_PBL_5', 'haslo_przedmiotowe_PBL_1', 'haslo_przedmiotowe_PBL_2', 'haslo_przedmiotowe_PBL_3', 'haslo_przedmiotowe_PBL_4', 'haslo_przedmiotowe_PBL_5']].reset_index(drop=True)
+# df_655 = df_655[df_655['decyzja'].isin(['margines', 'zmapowane'])][['X655', 'decyzja', 'dzial_PBL_1', 'dzial_PBL_2', 'dzial_PBL_3', 'dzial_PBL_4', 'dzial_PBL_5', 'haslo_przedmiotowe_PBL_1', 'haslo_przedmiotowe_PBL_2', 'haslo_przedmiotowe_PBL_3', 'haslo_przedmiotowe_PBL_4', 'haslo_przedmiotowe_PBL_5']].reset_index(drop=True)
+
+df_655 = df_655[df_655['decyzja'].isin(['zmapowane'])][['X655', 'decyzja', 'dzial_PBL_1', 'dzial_PBL_2', 'dzial_PBL_3', 'dzial_PBL_4', 'dzial_PBL_5', 'haslo_przedmiotowe_PBL_1', 'haslo_przedmiotowe_PBL_2', 'haslo_przedmiotowe_PBL_3', 'haslo_przedmiotowe_PBL_4', 'haslo_przedmiotowe_PBL_5']].reset_index(drop=True)
 
 dict_655 = {}
 for i, row in tqdm(df_655.iterrows(), total=df_655.shape[0]):
@@ -80,11 +81,12 @@ for i, row in tqdm(df_655.iterrows(), total=df_655.shape[0]):
 # dict_650['Poradnik']    
 # dict_655['Poradnik'] 
 #%% harvesting BN
-
+    
+#lista deskryptorów do wzięcia
 BN_descriptors = list(dict_650.keys())
 BN_descriptors.extend(dict_655.keys())
 BN_descriptors = list(set([re.sub('\$y.*', '', e) for e in BN_descriptors]))
- 
+#zakres lat 
 years = range(2013,2020)
    
 path = 'F:/Cezary/Documents/IBL/Migracja z BN/bn_all/2021-02-08/'
@@ -110,7 +112,8 @@ for file_path in tqdm(files):
             if year in years and bibliographic_level == 'm':
                 for el in sublist:
                     if el.startswith('=650') or el.startswith('=655'):
-                        if any(desc in el for desc in BN_descriptors):
+                        el = re.sub('\$y.*', '', el[10:]).replace('$2DBN', '')
+                        if any(desc == el for desc in BN_descriptors):
                             new_list.append(sublist)
                             break
         except ValueError:
@@ -126,52 +129,41 @@ for lista in new_list:
             slownik[el[1:4]] = el[6:]
     final_list.append(slownik)
 
-marc_df = pd.DataFrame(final_list).drop_duplicates().reset_index(drop=True)
-fields = marc_df.columns.tolist()
+df = pd.DataFrame(final_list).drop_duplicates().reset_index(drop=True)
+fields = df.columns.tolist()
 fields = [i for i in fields if 'LDR' in i or re.compile('\d{3}').findall(i)]
-marc_df = marc_df.loc[:, marc_df.columns.isin(fields)]
+df = df.loc[:, df.columns.isin(fields)]
 fields.sort(key = lambda x: ([str,int].index(type("a" if re.findall(r'\w+', x)[0].isalpha() else 1)), x))
-marc_df = marc_df.reindex(columns=fields)   
+df = df.reindex(columns=fields)   
+df_original = df.copy()
 
-BN_desc_and_decision_650 = [[re.sub('\$y.*', '', k), v['decyzja']] for k, v in dict_650.items()]
-BN_desc_and_decision_655 = [[re.sub('\$y.*', '', k), v['decyzja']] for k, v in dict_655.items()]
-BN_desc_and_decision = BN_desc_and_decision_650 + BN_desc_and_decision_655
-BN_desc_and_decision = list(set(BN_desc_and_decision))
-BN_desc_and_decision = [list(item) for item in set(tuple(row) for row in BN_desc_and_decision)]
-BN_desc_and_decision_df = pd.DataFrame(BN_desc_and_decision, columns=['deskryptor', 'decyzja'])
-
-marc_df_simple = marc_df[['001', '650', '655']]
-
-query = "select * from marc_df_simple a join BN_desc_and_decision_df b on a.'650' like '%'||b.deskryptor||'%'"
-test = pandasql.sqldf(query)
-query = "select * from marc_df_simple a join BN_desc_and_decision_df b on a.'655' like '%'||b.deskryptor||'%'"
-test2 = pandasql.sqldf(query)
-
-test = pd.concat([test, test2]).drop_duplicates()
-test = test[['001', 'decyzja']]
-test['decyzja'] = test.groupby('001')['decyzja'].transform(lambda x: '❦'.join(x.dropna().drop_duplicates().astype(str)))
-test = test.drop_duplicates().reset_index(drop=True)
-
-
-test = pd.merge(marc_df, test, on='001', how='left')
-
-margines = test[~test['decyzja'].str.contains('zmapowane')]
-zmapowane = test[test['decyzja'].str.contains('zmapowane')]
-
-
-margines.to_excel('libri_margines.xlsx', index=False)
-zmapowane.to_excel('libri_zmapowane.xlsx', index=False)
-
-
-
-
-dict_650['Gry fabularne$xstosowanie$xnauczanie']
+# =============================================================================
+# BN_desc_and_decision_650 = [[re.sub('\$y.*', '', k), v['decyzja']] for k, v in dict_650.items()]
+# BN_desc_and_decision_655 = [[re.sub('\$y.*', '', k), v['decyzja']] for k, v in dict_655.items()]
+# BN_desc_and_decision = BN_desc_and_decision_650 + BN_desc_and_decision_655
+# BN_desc_and_decision = list(set(BN_desc_and_decision))
+# BN_desc_and_decision = [list(item) for item in set(tuple(row) for row in BN_desc_and_decision)]
+# BN_desc_and_decision_df = pd.DataFrame(BN_desc_and_decision, columns=['deskryptor', 'decyzja'])
+# 
+# marc_df_simple = marc_df[['001', '650', '655']]
+# 
+# query = "select * from marc_df_simple a join BN_desc_and_decision_df b on a.'650' like '%'||b.deskryptor||'%'"
+# query1 = pandasql.sqldf(query)
+# query = "select * from marc_df_simple a join BN_desc_and_decision_df b on a.'655' like '%'||b.deskryptor||'%'"
+# query2 = pandasql.sqldf(query)
+# 
+# marc_df_simple = pd.concat([query1, query2]).drop_duplicates()[['001', 'decyzja', 'deskryptor']].rename(columns={'deskryptor': 'deskryptor z mapowania'})
+# marc_df_simple['deskryptor z mapowania'] = marc_df_simple.groupby('001')['deskryptor z mapowania'].transform(lambda x: '❦'.join(x.dropna().drop_duplicates().astype(str)))
+# marc_df_simple['decyzja'] = marc_df_simple.groupby('001')['decyzja'].transform(lambda x: '❦'.join(x.dropna().drop_duplicates().astype(str)))
+# marc_df_simple = marc_df_simple.drop_duplicates().reset_index(drop=True)
+# 
+# marc_df = pd.merge(marc_df, marc_df_simple, on='001', how='left')
+# =============================================================================
 
 #jakimi warunkami zmniejszyć liczbę rekordów?
 
-#%% live coding z Karoliną
-df = pd.read_excel('libri_zmapowane.xlsx')
-
+#%% dalsze filtrowanie
+#odsianie rekordów, które nie mają nic wspólnego z PL
 def czy_polonik(x):
     polish_language = x['008'][35:38] == 'pol'
     published_in_Poland = x['008'][15:17] == 'pl'
@@ -197,11 +189,76 @@ def czy_polonik(x):
         return False
     
 df['czy polonik'] = df.apply(lambda x: czy_polonik(x), axis=1)
-
-#nie_polonik = df[df['czy polonik'] == False]
-
 df = df[df['czy polonik'] == True]
-df.to_excel('libri_zmapowane2.xlsx', index=False)
+
+#odsianie druków ulotnych
+druki_ulotne = df[(df['380'].str.contains('Druki ulotne')) & (df['380'].notnull())]['001'].to_list()
+df = df[~df['001'].isin(druki_ulotne)]
+
+#gatunki literackie
+deskryptory_spoza_centrum = get_as_dataframe(sheet.worksheet('deskryptory_spoza_centrum'), evaluate_formulas=True).dropna(how='all').dropna(how='all', axis=1)['deskryptor'].to_list()
+deskryptory_spoza_centrum = list(set([re.sub('\$y.*', '', e) for e in deskryptory_spoza_centrum]))
+list_655 = list(set([re.sub('\$y.*', '', e) for e in dict_655.keys()]))
+list_655 = [e for e in list_655 if e not in deskryptory_spoza_centrum]
+
+def gatunki_literackie(x):
+    for field in x:
+        if pd.notnull(field):
+            for element in field.split('❦'):
+                element = re.sub('\$y.*', '', element[4:]).replace('$2DBN', '')
+                if any(desc == element for desc in list_655):
+                    return True
+    return False
+
+df['gatunki literackie'] = df[['650', '655']].apply(lambda x: gatunki_literackie(x), axis=1)
+#zostawiamy tylko książki lub dobre gatunki literackie
+
+#TUTAJ!!!!!!!!!!! podjąć decyzję
+
+df = df[(df['380'].str.lower().str.contains('książ|book', regex=True)) | (df['380'].isnull())]
+rekordy_id_bez_gatunkow_literackich = df['001'].drop_duplicates().to_list()
+
+df = df[(df['380'].str.lower().str.contains('książ|book', regex=True)) | (df['380'].isnull()) | (df['gatunki literackie'] == True)]
+rekordy_id_z_gatunkami_literackimi = df['001'].drop_duplicates().to_list()
+
+roznica = list(set(rekordy_id_z_gatunkami_literackimi) - set(rekordy_id_bez_gatunkow_literackich))
+roznica_df = df_original[df_original['001'].isin(roznica)]
+roznica_df.to_excel('roznica.xlsx', index=False)
+
+#%% dobre gatunki literackie
+#jeśli coś ma gatunek literacki, to jest z automatu dobre
+dobre_df = df[df['gatunki literackie'] == True]
+
+#pozostałe rekordy przetwarzamy
+df = df[~df['001'].isin(dobre_df['001'])]
+
+#%% frekwencje
+
+deskryptory_z_df = df[['001', '655', '650']].reset_index(drop=True)
+
+deskryptory_dict = {}
+for i, row in tqdm(deskryptory_z_df.iterrows(), total=deskryptory_z_df.shape[0]):
+    lista_deskryptorow = [e for e in f"{row['650']}❦{row['655']}".split('❦') if e != 'nan']
+    deskryptory_dict[row['001']] = {'deskryptory w rekordzie': lista_deskryptorow, 'liczba deskryptorów': len(lista_deskryptorow), 'liczba dobrych deskryptorów': 0}
+    
+    for el in deskryptory_dict[row['001']]['deskryptory w rekordzie']:
+        if any(desc in el for desc in BN_descriptors):
+            deskryptory_dict[row['001']]['liczba dobrych deskryptorów'] += 1
+            
+    deskryptory_dict[row['001']]['procent dobrych deskryptorów'] = deskryptory_dict[row['001']]['liczba dobrych deskryptorów']/deskryptory_dict[row['001']]['liczba deskryptorów']
+
+
+
+test = pd.DataFrame.from_dict(deskryptory_dict, orient='index')
+test.to_excel('procent dobrych deskryptorów.xlsx')    
+    
+    any(desc in el for desc in BN_descriptors)
+# % zmapowanych deskryptorów dla listy deskryptorów w rekordzie
+[print(e) for e in BN_descriptors if e.startswith('Podręcznik')]
+
+
+# frekwencje deskryptorów w zbiorze
+
 
 #twarda lista gatunków, które idą na 100%
 #dodać warunek z listy deskryptorów 655
--- translations_nor_national_library_harvesting.py
+++ translations_nor_national_library_harvesting.py
@@ -1,280 +1,14 @@
-#original code by Ondrej Vimr
-from oaipmh.client import Client
-from oaipmh.metadata import MetadataRegistry
-from pymarc import marcxml, MARCReader
-import io
-from lxml.etree import tostring
-from datetime import datetime, timedelta
-import sys
-import os
-import glob
-import regex as re
-import pandas as pd
-from tqdm import tqdm
-
-#%%harvesting OAI-PMH
-
-#date
-now = datetime.now()
-year = now.year
-month = '{:02d}'.format(now.month)
-day = '{:02d}'.format(now.day)
-
-# vars
-
-savefile = f'F:/Cezary/Documents/IBL/Translations/nor_{year}-{month}-{day}.marc'
-logfile = f'F:/Cezary/Documents/IBL/Translations/nor_log_{year}-{month}-{day}.txt'
-# savefile = f'C:/Users/User/Desktop/nor_{year}-{month}-{day}.marc'
-# logfile = f'C:/Users/User/Desktop/nor_log_{year}-{month}-{day}.txt'
-URL = 'http://bibsys.alma.exlibrisgroup.com/view/oai/47BIBSYS_NB'
-start_date = '2012-12-20 00:00:00'   # [YYYY-mm-dd HH:MM:SS]
-stop_date = datetime.now().replace(minute=0, hour=0, second=0, microsecond=0).strftime("%Y-%m-%d %H:%M:%S")
-
-# defs
-
-def MarcXML(xml):
-    handler = marcxml.XmlHandler()
-    data = tostring(xml, encoding='UTF-8')
-    marcxml.parse_xml(io.BytesIO(data), handler)
-    return handler.records[0]
-
-def valid_date(s):
-    try:
-        return datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
-    except:
-        print('Invalid date format.')
-        sys.exit(1)
-
-def saverecords(recs):
-    for record in recs:
-        metadata = record[1]
-        with open(savefile, 'ab') as out: 
-            out.write(metadata.as_marc())
-    with open(logfile, 'a') as log:
-        log.write(f'{from_date} {until_date}\n')
-    print(from_date, until_date)
-    return
-
-# init
-
-registry = MetadataRegistry()
-registry.registerReader('marc21', MarcXML)
-client = Client(URL, registry)
-
-start = valid_date(start_date)
-start = valid_date('2020-01-21 00:00:00')
-stop = valid_date(stop_date)
-
-# main
-
-while start < stop:
-    from_date = start
-    start = start + timedelta(days=1)  # increase days one by one
-    until_date = start
-    try: 
-        records = client.listRecords(metadataPrefix='marc21', set='oai_komplett', from_=from_date, until=until_date)
-        saverecords(records)
-    except: pass # skipping deleted entries
-
-print('Done!')
-
-for record in records:
-    print(record)
-
-#https://bibsys.alma.exlibrisgroup.com/view/oai/47BIBSYS_NB/request?verb=ListRecords&metadataPrefix=marc21&set=oai_komplett&from=2020-01-21T00:00:00Z&until=2020-01-22T00:00:00Z
-
-
-
 import requests
 from pymarc import MARCReader
 from my_functions import xml_to_mrk, xml_to_mrc
 import pymarc
 import xml.etree.ElementTree as et
-
-url = 'https://bibsys.alma.exlibrisgroup.com/view/oai/47BIBSYS_NB/request?verb=ListRecords&metadataPrefix=marc21&set=oai_komplett&from=2020-01-21T00:00:00Z&until=2020-01-22T00:00:00Z'
-response = requests.get(url)
-with open('test2.xml', 'wb') as file:
-    file.write(response.content)
-    
-tree = et.parse('test2.xml')
-root = tree.getroot() 
-ns = '{http://www.openarchives.org/OAI/2.0/}'
-root.tag
-records = root.findall(f'.//{ns}record/{ns}metadata/{{http://www.loc.gov/MARC21/slim}}record')
-
-list_of_marks = []
-for r in tqdm(records):
-    r = et.tostring(r)
-    with open('test3.xml', 'wb') as file:
-        file.write(b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-        file.write(r)
-    xml_to_mrk('test3.xml', 'test3.mrk')
-    marc_list = io.open('test3.mrk', 'rt', encoding = encoding).read().splitlines()
-    list_of_marks.append(marc_list)
-
-
-#ogarnąć nextpage i resumptionToken
-
-
-
-
-context = et.iterparse('test2.xml', events=('end', ))
-for event, elem in context:
-    print(elem.tag)
-
-
-
-
-
-
-
-
-
-writer = pymarc.TextWriter(io.open('testtest.mrk', 'wt', encoding="utf-8"))
-for record in records:
-    pymarc.map_xml(writer.write, str(record)) 
-    writer.close() 
-    
-
-
-def xml_to_mrk(path_in, path_out):
-    writer = pymarc.TextWriter(io.open(path_out, 'wt', encoding="utf-8"))
-    pymarc.map_xml(writer.write, path_in) 
-    writer.close() 
-    
-    
-lista = []
-for record in record
-
-records[0].getchildren()
-
-root.getchildren()
-
-
-
-
-
-
-
-
-
-
-
-
-for e in root[-1].getchildren():
-    print(e)
-    with open('test3.xml', 'wb') as file:
-        file.write(et.tostring(e))
-
-root[-1].getchildren()[0].getchildren()[-1].getchildren()[0].getchildren()
-
-lista = []
-for e in root[-1].getchildren():
-    try:
-        val = e.getchildren()[-1]
-        lista.append(et.tostring(val))
-    except IndexError:
-        pass
-    dotrzeć do rekordów i je poiterować - może da się z nich zrobić marki
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-tree = et.parse('test.xml')
-root = tree.getroot()
-
-dir(root)
-
-print(root[-1])
-
-for e in root[-1].getchildren():
-    e = MARCReader(et.tostring(e))
-    print(e)
-
-test = MARCReader(et.tostring(root[-1]))
-next(test)
-for e in root:
-    print(e)
-
-with open('test2.xml', 'wb') as f:
-    for e in root[-1].getchildren():
-        f.write(et.tostring(e))
-
-xml_to_mrk('test2.xml', 'test.mrk')   
-
-
-
-
-
-
-
-for e in tree.iter():
-    print(e)
-    
-writer = pymarc.TextWriter(io.open('test.mrk', 'wt', encoding="utf-8"))    
-records = pymarc.map_xml(writer.write, 'test.xml')     
-    
-xml_to_mrk('test.xml', 'test.mrk')    
-xml_to_mrc('test.xml', 'test.mrc')  
-xml_to   
-
-
-def xml_to_mrk(path_in, path_out):
-    writer = pymarc.TextWriter(io.open(path_out, 'wt', encoding="utf-8"))
-    records = pymarc.map_xml(writer.write, path_in) 
-    writer.close() 
-
-
-def xml_to_mrc(path_in, path_out):
-    writer = pymarc.MARCWriter(open(path_out, 'wb'))
-    records = pymarc.map_xml(writer.write, path_in) 
-    writer.close()  
-
-import lxml.etree
-# tree_pretty = lxml.etree.parse('test.xml')
-# pretty = lxml.etree.tostring(tree_pretty, encoding="unicode", pretty_print=True)
-
-
-
-
-
-
-
-
-   
-    
-
-reader = MARCReader(response.content, force_utf8=True)
-
-
-(marc_target, to_unicode=True, force_utf8=False,
-hide_utf8_warnings=False, utf8_handling=’strict’,
-file_encoding=’iso8859-1’, permissive=False)
-for record in reader:
-    print(1)
-with open('file.mrc', 'wb') as out:
-   out.write(reader.as_marc())
-
-for record in reader:
-    with open('file.mrc', 'wb') as out:
-        out.write(record.as_marc())
-
-
-import urllib.request
-from datetime import datetime, timedelta
 import sys
+from datetime import datetime, timedelta
+from tqdm import tqdm
+import io
 
+encoding = 'UTF-8'
 start_date = '2018-02-21 00:00:00'   # [YYYY-mm-dd HH:MM:SS]
 stop_date = '2021-04-01 00:00:00'
 
@@ -288,33 +22,46 @@ def valid_date(s):
 start = valid_date(start_date)
 stop = valid_date(stop_date)
 
+list_of_records = []
+ns = '{http://www.openarchives.org/OAI/2.0/}'
+
 while start < stop:
     from_date = start
-    start = start + timedelta(days=1)  # increase days one by one
+    print(from_date.strftime('%Y-%m-%dT%H:%M:%SZ'))
+    start = start + timedelta(days=1)
     until_date = start
-    print(from_date)
-    harvesturl = 'https://bibsys.alma.exlibrisgroup.com/view/oai/47BIBSYS_NB/request?verb=ListRecords&metadataPrefix=marc21&set=oai_komplett&from='+from_date.strftime('%Y-%m-%dT%H:%M:%SZ')+'&until='+until_date.strftime('%Y-%m-%dT%H:%M:%SZ')
-    try:
-        with urllib.request.urlopen(harvesturl) as f:
-            urlcontent = f.read().decode('utf-8')
-            allcontent = urlcontent
-            while 'resumptionToken>' in urlcontent:
-                #print('token now') # monitoring tokens                
-                rtoken = urlcontent.split('resumptionToken>')[1][:-2]
-                harvesturl = 'https://bibsys.alma.exlibrisgroup.com/view/oai/47BIBSYS_NB/request?verb=ListRecords&resumptionToken='+rtoken
-                #print(harvesturl)
-                try:
-                    with urllib.request.urlopen(harvesturl) as t:
-                        urlcontent = t.read().decode('utf-8')
-                        allcontent+=urlcontent
-                except urllib.error.URLError as e:
-                    print(e.reason)
-            #print(allcontent)
-            saverecords(allcontent)# harvest of the day here - please save me
-    except urllib.error.URLError as e:
-        print(e.reason)
+    
+    url = f"https://bibsys.alma.exlibrisgroup.com/view/oai/47BIBSYS_NB/request?verb=ListRecords&metadataPrefix=marc21&set=oai_komplett&from={from_date.strftime('%Y-%m-%dT%H:%M:%SZ')}&until={until_date.strftime('%Y-%m-%dT%H:%M:%SZ')}"
+    response = requests.get(url)
+    root = et.fromstring(response.content)
+    records = root.findall(f'.//{ns}record/{ns}metadata/{{http://www.loc.gov/MARC21/slim}}record')
+    
+    for r in records:
+        r = et.tostring(r)
+        with open('test.xml', 'wb') as file:
+            file.write(b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+            file.write(r)
+        xml_to_mrk('test.xml', 'test.mrk')
+        marc_list = io.open('test.mrk', 'rt', encoding = encoding).read().splitlines()
+        list_of_records.append(marc_list)
+    
+    while 'resumptionToken>' in response.content.decode('utf-8'):
+        rtoken = response.content.decode('utf-8').split('resumptionToken>')[1][:-2]
+        url = f"https://bibsys.alma.exlibrisgroup.com/view/oai/47BIBSYS_NB/request?verb=ListRecords&resumptionToken={rtoken}"
+        response = requests.get(url)
+        root = et.fromstring(response.content)
+        records = root.findall(f'.//{ns}record/{ns}metadata/{{http://www.loc.gov/MARC21/slim}}record')
+        
+        for r in records:
+            r = et.tostring(r)
+            with open('test.xml', 'wb') as file:
+                file.write(b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+                file.write(r)
+            xml_to_mrk('test.xml', 'test.mrk')
+            marc_list = io.open('test.mrk', 'rt', encoding = encoding).read().splitlines()
+            list_of_records.append(marc_list)
+        
 
-print('done')
 
 
 
