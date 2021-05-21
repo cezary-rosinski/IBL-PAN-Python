867bceed7ca3d62e17a771d966401c28e18d20ae
Cezary <cezary.rosinski@gmail.com>
2021-05-21 18:54:09 +0200

    21.05.2021; home

--- garmin_statistics.py
+++ garmin_statistics.py
@@ -51,10 +51,12 @@ for i, el in enumerate(act):
     zones_1_2 = sum([e['secsInZone'] for e in hr_timezones if e['zoneNumber'] in [1,2]])/total_time
     act[i].append(zones_1_2)
     
-df = pd.DataFrame(act, columns=['activityId', 'startTimeLocal', 'distance', 'averageHR', 'total time', 'zones 1 and 2 %'])
+df = pd.DataFrame(act, columns=['activityId', 'date', 'distance', 'averageHR', 'total time', 'zones 1 and 2 %'])
 
-df['date'] = df['startTimeLocal'].apply(lambda x: x[:10])
-df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+# df['date'] = df['startTimeLocal'].apply(lambda x: x[:10])
+# df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+
+# df['date'] = pd.to_datetime(df['startTimeLocal'], format='%Y-%m-%d %H:%M:%S')
 df['s/km'] = df['total time']*1000/df['distance']
 
 def rosinski_hubar_index(x):
@@ -62,12 +64,10 @@ def rosinski_hubar_index(x):
     
 df['rosinski-hubar index'] = df.apply(lambda x: rosinski_hubar_index(x), axis=1)
 
-df.to_excel('running.xlsx', index=False)
+# df.to_excel('running.xlsx', index=False)
 
-fig = df.groupby('date').sum()['rosinski-hubar index'].plot(figsize = (10,10), title='running').get_figure()
+fig = df.groupby('date').sum()['rosinski-hubar index'].plot(figsize = (40,10), title='running', legend=True, grid=True).get_figure()
 fig.savefig('running.jpg')
-
-
 
 
 
