3173e6c48afd14d37e318f52a5ac2eecde0ade7d
Cezary <cezary.rosinski@gmail.com>
2022-05-14 13:00:58 +0200

    2022-05-14;home

--- kindle_clippings.py
+++ kindle_clippings.py
@@ -4,18 +4,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-filepath = "H:\documents\My Clippings.txt"
+filepath = "I:\documents\My Clippings.txt"
 
-browser = webdriver.Firefox()
+browser = webdriver.Firefox(executable_path='geckodriver.exe')
 browser.get('https://my.clippings.io/#/login')
 
-username_input = browser.find_element_by_xpath("//input[@placeholder='Email or username']")
-password_input = browser.find_element_by_xpath("//input[@placeholder='Password']")
+username_input = browser.find_element(by=By.XPATH, value="//input[@placeholder='Adres e-mail lub nazwa użytkownika']")
+password_input = browser.find_element(by=By.XPATH, value="//input[@placeholder='Hasło']")
 
 username_input.send_keys(clippings_username)
 password_input.send_keys(clippings_password)
 
-login_button = browser.find_element_by_xpath("//button[@data-cy='loginbutton']")
+login_button = browser.find_element(by=By.XPATH, value="//button[@data-cy='loginbutton']")
 login_button.click()
 
 # import_button = browser.find_element_by_id('importbutton')
@@ -23,7 +23,7 @@ login_button.click()
 
 browser.get('https://my.clippings.io/import/file')
 
-upload_input = browser.find_element_by_xpath("//input[@type='file']")
+upload_input = browser.find_element(by=By.XPATH, value="//input[@type='file']")
 upload_input.send_keys(filepath)
 
 WebDriverWait(browser, 30).until(
