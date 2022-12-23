c38b533f885d68aeedb161d1322a5adfcda01ecf
Cezary Rosiński <cezary.rosinski@gmail.com>
2022-12-23 14:10:25 +0100

    laptop

--- freeCodeCamp tasks.py
+++ freeCodeCamp tasks.py
@@ -103,3 +103,98 @@ There should be four spaces between each problem.
 There should be dashes at the bottom of each problem. The dashes should run along the entire length of each problem individually. (The example above shows what this should look like.)'''
 
 #%% 2
+
+start_time, time_to_add, start_day = "6:30 PM", "205:12", 'sunday'
+
+def add_time(start_time, time_to_add, start_day=None):
+    week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    start_hours = int(start_time.split(':')[0])
+    start_minutes = int(start_time.split(':')[-1].split(' ')[0])
+    start_in_minutes = start_hours * 60 + start_minutes
+    start_period = start_time.split(':')[-1].split(' ')[-1]
+    other_period = [e for e in ['AM', 'PM'] if e != start_period][0]
+    
+    add_time_in_minutes = int(time_to_add.split(':')[0]) * 60 + int(time_to_add.split(':')[-1])
+    
+    total_time_in_minutes = start_in_minutes + add_time_in_minutes
+    final_hours = total_time_in_minutes // 60
+    
+    whole_day_in_minutes = 24 * 60
+    if start_period == 'AM':
+        how_many_days = total_time_in_minutes / whole_day_in_minutes
+    else:
+        how_many_days = (total_time_in_minutes + 12 * 60) / whole_day_in_minutes
+    
+    hours = [12 for e in range(final_hours // 12)]
+    hours.append(final_hours % 12 if final_hours % 12 != 0 else 12)
+    
+    if len(hours) % 2 == 0:
+        final_period = other_period
+    else: final_period = start_period
+    
+    final_minutes = '{:02d}'.format(total_time_in_minutes % 60)
+    
+    days_later = int(how_many_days // 1)
+    if days_later == 0 and not start_day:
+        final_time = f'{hours[-1]}:{final_minutes} {final_period}'
+    elif days_later == 0 and start_day:
+        final_day = start_day
+        final_time = f'{hours[-1]}:{final_minutes} {final_period}, {final_day}' 
+    elif days_later == 1 and start_day:
+        final_day = '(next day)'
+        final_week_day = [week[i+days_later] for i, e in enumerate(week) if e.lower() == start_day.lower()][0]
+        final_time = f'{hours[-1]}:{final_minutes} {final_period}, {final_week_day} {final_day}'
+    elif days_later > 1 and start_day:
+        final_day = f'({days_later} days later)'
+        final_week_day = [week[i+days_later] if i+days_later <= len(week) else week[(i+days_later)%7] for i, e in enumerate(week) if e.lower() == start_day.lower()][0]
+        final_time = f'{hours[-1]}:{final_minutes} {final_period}, {final_week_day} {final_day}'
+    elif days_later == 1:
+        final_day = '(next day)'
+        final_time = f'{hours[-1]}:{final_minutes} {final_period} {final_day}' 
+    else:
+        final_day = f'({days_later} days later)'
+        final_time = f'{hours[-1]}:{final_minutes} {final_period} {final_day}' 
+    
+    return final_time
+
+
+
+
+add_time("3:00 PM", "3:10")
+# Returns: 6:10 PM
+
+add_time("11:30 AM", "2:32", "Monday")
+# Returns: 2:02 PM, Monday
+
+add_time("11:43 AM", "00:20")
+# Returns: 12:03 PM
+
+add_time("10:10 PM", "3:30")
+# Returns: 1:40 AM (next day)
+
+add_time("11:43 PM", "24:20", "tueSday")
+# Returns: 12:03 AM, Thursday (2 days later)
+
+add_time("6:30 PM", "205:12", 'sunday')
+# Returns: 7:42 AM (9 days later)
+
+add_time("8:16 PM", "466:02", "tuesday")
+#"6:18 AM, Monday (20 days later)"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
