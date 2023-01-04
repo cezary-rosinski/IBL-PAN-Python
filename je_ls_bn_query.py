994e022c1b035d04bff181ca71f4705ac7c9b687
Cezary <cezary.rosinski@gmail.com>
2023-01-04 18:21:15 +0100

    home

---
+++ je_ls_bn_query.py
@@ -0,0 +1,27 @@
+import requests
+
+url = 'https://data.bn.org.pl/api/networks/bibs.json?'
+params = {'limit': 100, 'marc': '773t Literatura na Świecie'}
+
+
+result = requests.get(url, params=params)
+result = result.json()
+next_page = result.get('nextPage')
+
+start_url = 'https://data.bn.org.pl/api/networks/bibs.marc?limit=100&amp;marc=773t+Literatura+na+%C5%9Awiecie'
+
+
+ls_response = requests.get(start_url).text
+progress = 1
+while next_page:
+    print(progress)
+    new_next_url = next_page.replace('json?', 'marc?')
+    temp_response = requests.get(new_next_url).text
+    ls_response += temp_response
+    result = requests.get(next_page)
+    result = result.json()
+    next_page = result.get('nextPage')
+    progress += 1
+    
+with open('ls_bn.mrc', 'w', encoding='utf-8') as file:
+    file.write(ls_response)
