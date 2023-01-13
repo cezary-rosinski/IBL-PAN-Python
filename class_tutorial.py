c16e50fb7e629985a54ea8589effceeed988d77d
Cezary <cezary.rosinski@gmail.com>
2023-01-13 10:56:55 +0100

    class inheritance

--- class_tutorial.py
+++ class_tutorial.py
@@ -290,32 +290,109 @@ my_date = datetime.date(2016, 7, 11)
 
 print(Employee.is_workday(my_date))
 
-#%% https://www.youtube.com/watch?v=RSl87lqOXDE creating subclasses
+#%% https://www.youtube.com/watch?v=RSl87lqOXDE creating subclasses + inheritance
+
+class Employee:
+    
+    raise_amt = 1.04
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+        
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+#method resolution order        
+class Developer(Employee):
+    pass
+
+dev_1 = Developer('Corey', 'Schafer', 50000)
+dev_2 = Employee('Test', 'User', 60000)
+
+# print(dev_1.email)
+# print(dev_2.email)
+
+print(help(Developer))
+
+#%%
+
+class Developer(Employee):
+    raise_amt = 1.10
+
+dev_1 = Developer('Corey', 'Schafer', 50000)
+dev_2 = Developer('Test', 'User', 60000)
+
+print(dev_1.pay)
+dev_1.apply_raise()
+print(dev_1.pay)
+
+#%% inheritance
+
+class Developer(Employee):
+    raise_amt = 1.10
+    
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+dev_2 = Developer('Test', 'User', 60000, 'Java')
+
+print(dev_1.email)
+print(dev_1.prog_lang)
+
+#%%
+
+class Manager(Employee):
+    
+    def __init__(self, first, last, pay, employees=None): #nigdy nie wpisywać domyślnie struktur danych w argumentach, dlatego tutaj nie daje pustej listy
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+            
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+            
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+            
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+print(mgr_1.email)
+
+mgr_1.add_emp(dev_2)
+
+mgr_1.print_emps()
+
+mgr_1.remove_emp(dev_1)
+mgr_1.print_emps()
 
 
+#%%
 
+print(isinstance(mgr_1, Manager))
+print(isinstance(mgr_1, Employee))
+print(isinstance(mgr_1, Developer))
 
+print(issubclass(Developer, Employee))
+print(issubclass(Manager, Employee))
+print(issubclass(Manager, Developer))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#%% https://www.youtube.com/watch?v=3ohzBxoFHAY Python OOP Tutorial 5: Special (Magic/Dunder) Methods
 
 
 
