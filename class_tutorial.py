6715f38f2bf6dfc3f7b5a8e302cf6c4cfdb0ca58
Cezary <cezary.rosinski@gmail.com>
2023-01-13 16:38:16 +0100

    dunder methods

--- class_tutorial.py
+++ class_tutorial.py
@@ -395,6 +395,87 @@ print(issubclass(Manager, Developer))
 #%% https://www.youtube.com/watch?v=3ohzBxoFHAY Python OOP Tutorial 5: Special (Magic/Dunder) Methods
 
 
+class Employee:
+    
+    raise_amt = 1.04
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+        
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+        
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+repr(emp_1)
+str(emp_1)
+#%%
+class Employee:
+    
+    raise_amt = 1.04
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+        
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+        
+    def __repr__(self):
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+    
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
+    
+    def __add__(self, other):
+        return self.pay + other.pay
+    
+    def __len__(self):
+        return len(self.fullname())
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+repr(emp_1)
+str(emp_1)
+
+print(emp_1.__repr__())
+print(emp_1.__str__())
+
+
+print(int.__add__(1, 2))
+print(str.__add__('a', 'b'))
+
+print(emp_1 + emp_2)
+
+print(len('test'))
+print('test'.__len__())
+
+print(len(emp_1))
+
+#%% https://www.youtube.com/watch?v=jCzT9XFZ5bw Python OOP Tutorial 6: Property Decorators - Getters, Setters, and Deleters
+
+
+
+
+
+
+
+
+
+
+
 
 
 
