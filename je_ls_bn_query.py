e432b571e7a321f45951b6671a258c3707630458
Cezary <cezary.rosinski@gmail.com>
2023-02-08 18:40:01 +0100

    je new bn query

--- je_ls_bn_query.py
+++ je_ls_bn_query.py
@@ -1,14 +1,16 @@
 import requests
 
 url = 'https://data.bn.org.pl/api/networks/bibs.json?'
-params = {'limit': 100, 'marc': '773t Literatura na Świecie'}
+# params = {'limit': 100, 'marc': '773t Literatura na Świecie'}
+params = {'limit': 100, 'subject': 'Borges, Jorge Luis, 1899-1986'}
 
 
 result = requests.get(url, params=params)
 result = result.json()
 next_page = result.get('nextPage')
 
-start_url = 'https://data.bn.org.pl/api/networks/bibs.marc?limit=100&amp;marc=773t+Literatura+na+%C5%9Awiecie'
+# start_url = 'https://data.bn.org.pl/api/networks/bibs.marc?limit=100&amp;marc=773t+Literatura+na+%C5%9Awiecie'
+start_url = 'https://data.bn.org.pl/api/networks/bibs.marc?limit=100&amp;subject=Borges%2C+Jorge+Luis%2C+1899-1986'
 #link powyżej też trzeba edytować; najlepiej zaznaczając w API BN format marc i skopiować URL
 
 
@@ -24,5 +26,7 @@ while next_page:
     next_page = result.get('nextPage')
     progress += 1
     
-with open('ls_bn.mrc', 'w', encoding='utf-8') as file:
+with open('borges_temat_bn.mrc', 'w', encoding='utf-8') as file:
     file.write(ls_response)
+    
+# mrk_df.to_excel('borges_temat_bn.xlsx', index=False)
