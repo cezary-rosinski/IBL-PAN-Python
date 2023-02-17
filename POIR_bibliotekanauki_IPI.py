cd8a4ff6601085c4868eb6c325e88f3da27b22a5
Cezary <cezary.rosinski@gmail.com>
2023-02-17 15:21:45 +0100

    poir bn ipi

---
+++ POIR_bibliotekanauki_IPI.py
@@ -0,0 +1,111 @@
+from sickle import Sickle
+import xml.etree.cElementTree as ET
+import requests
+from bs4 import BeautifulSoup
+from glob import glob
+from tqdm import tqdm
+import fitz
+import regex as re
+from requests_html import AsyncHTMLSession
+
+#%% def
+asession = AsyncHTMLSession()
+async def get_sets(url):
+    response = await asession.get(url)
+    await response.html.arender()
+    return response.html.xpath("//body/div[@id='root']/div[@id='main-container']/main[@id='main-content']/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]")
+
+#%% main
+
+paths = [r'F:\Cezary\Documents\IBL\IPI PAN\1000 anotowanych artykułów naukowych/', r'F:\Cezary\Documents\IBL\IPI PAN\1000 anotowanych artykułów naukowych\dodane 31.01.2023/']
+files = []
+for path in paths:
+    files.append([f for f in glob(path + '*.pdf', recursive=True)])
+
+files = [e for sub in files for e in sub]
+
+oai_url = 'https://bibliotekanauki.pl/api/oai/articles'
+sickle = Sickle(oai_url)
+
+bibliotekanauki_articles = []
+errors = []
+
+for file in tqdm(files):
+    bn_id = re.findall('\d+', file.split('\\')[-1])[0]
+    
+    record = sickle.GetRecord(identifier=f'oai:bibliotekanauki.pl:{bn_id}', metadataPrefix='jats')
+    try:
+        article_dict = {'file': file.split('\\')[-1],
+                        'title': record.metadata.get('article-title'),
+                        'subject': record.metadata.get('subject'),
+                        'journal': record.metadata.get('journal-title'),
+                        'keywords': record.metadata.get('kwd')}
+    except AttributeError:
+        bibliotekanauki_articles.append(article_dict)
+        errors.append(file.split('\\')[-1])
+
+
+    
+#%% notes
+doc = fitz.open(file)
+page = doc[0]
+annot = list(page.annots())[-1]
+annot.info
+annot.get_text()
+
+
+
+
+
+
+
+
+url = "https://bibliotekanauki.pl/search?page=1&q=%2522Wpływ udziału społeczeństwa w procedurze oceny oddziaływania na środowisko na rozwój inwestycji infrastrukturalnych%2522"
+r = requests.get(url)
+r.content
+
+soup = BeautifulSoup(r.content, 'html.parser')
+
+
+
+
+
+
+test = await get_sets(url)
+
+test[0].text
+
+test.html.xpath("//body/div[@id='root']/div[@id='main-container']/main[@id='main-content']/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]")
+    
+        
+    domain_list = [domain.text for domain in response.html.find('span.scientificFieldHeader')]
+    if search_domain in domain_list:
+        hum_sets.append(set_spec) 
+await get_sets() # await because IPython kernel is running own loop
+
+
+record.header.identifier
+record.metadata
+record.xml
+record.raw
+
+dir(record)
+
+from xml.dom import minidom
+xmlstr = minidom.parseString(ET.tostring(record.xml)).toprettyxml(indent="   ")
+print(xmlstr)
+
+
+
+sickle = Sickle('https://bibliotekanauki.pl/api/oai/articles')
+record = sickle.GetRecords('')
+
+records = sickle.ListRecords(metadataPrefix='oai_dc')
+records.next()
+<Record oai:eprints.rclis.org:4088>
+
+
+dir(sickle)
+
+
+https://bibliotekanauki.pl/api/oai/articles?verb=GetRecord&metadataPrefix=jats&identifier=oai:bibliotekanauki.pl:887601
