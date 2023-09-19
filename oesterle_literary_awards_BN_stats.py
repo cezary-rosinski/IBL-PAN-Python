4cd89be5e485a856f1d88a53cf92038cd10bf8db
Cezary Rosiński <cezary.rosinski@gmail.com>
2023-09-19 15:00:57 +0200

    final graphs

--- oesterle_literary_awards_BN_stats.py
+++ oesterle_literary_awards_BN_stats.py
@@ -15,6 +15,7 @@ with open(path, 'rt', encoding='utf-8') as f:
     data = json.load(f)
 data.pop('polish literature')
     
+# test = {k:v for k,v in data.items() if k in ['belarusian literature', 'ukrainian literature', 'austrian literature', 'german literature', 'french literature', 'spanish literature', 'japanese literature']}
 
 test = {k:v for k,v in data.items() if k in ['belarusian literature', 'ukrainian literature', 'austrian literature', 'american literature', 'german literature', 'french literature', 'spanish literature', 'japanese literature']}
 
@@ -66,7 +67,37 @@ for i, (literature, values) in tqdm(enumerate(test.items()), total = len(test)):
 plt.tight_layout()
 plt.show()
 
+# Tworzenie wykresu z udziałem literatury przedmiotowej lub odsetkiem literatury przedmiotowej
+ttt = {k:v for k,v in test.items() if k == 'austrian literature'}
+fig, axes = plt.subplots(nrows=len(test), ncols=1, figsize=(10, 6 * len(test)), dpi=200)
 
+for i, (literature, values) in tqdm(enumerate(test.items()), total = len(test)):
+    i = 0
+    literature, values = tuple(test.items())[i]
+    years = list(values.keys())
+    literature_values = [entry["literature"] for entry in values.values()]
+    secondary_values = [entry["secondary"] for entry in values.values()]
+    
+    graph_values = [l + s for l, s in zip(literature_values, secondary_values)]
+
+    sorted_indices = sorted(range(len(years)), key=lambda k: years[k])
+    years = [years[i] for i in sorted_indices]
+    literature_values = [literature_values[i] for i in sorted_indices]
+    # secondary_values = [round(secondary_values[i]/graph_values[i], 2) for i in sorted_indices]   
+    secondary_values = [round(secondary_values[i]/literature_values[i], 2) for i in sorted_indices]  
+
+    ax = axes[i]
+    # ax.bar(years, literature_values, label='Literature')
+    ax.bar(years, secondary_values, label='Secondary', alpha=0.5)
+    ax.set_title(f'Percentage of the secondary literature in {literature}')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Percentage of the secondary literature')
+    ax.legend()
+    
+    ax.set_xticklabels(years, rotation=90)
+
+plt.tight_layout()
+plt.show()
 
 # Przygotowanie danych do wykresu scatter plot
 scatter_data = []
@@ -104,12 +135,69 @@ plt.xticks(rotation=90)
 
 plt.show()
 
+## prace 19.09.2023
+# Calculate total summed secondary literature and summed literature for each language
+summed_secondary = {}
+summed_literature = {}
+
+for language, years_data in test.items():
+    summed_secondary[language] = sum(year_data['secondary'] for year_data in years_data.values())
+    summed_literature[language] = sum(year_data['literature'] for year_data in years_data.values())
+
+# Calculate the quotient
+quotient = {language: summed_secondary[language] / summed_literature[language] for language in test.keys()}
+
+# Create a bar chart
+languages = list(quotient.keys())
+quotients = list(quotient.values())
+
+plt.figure(figsize=(10, 6))
+plt.bar(languages, quotients, color='skyblue')
+plt.xlabel('Language')
+plt.ylabel('Quotient (Secondary Literature / Literature)')
+plt.title('Quotient of Secondary Literature divided by Literature')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+plt.show()
 
 
+# Przygotowanie danych do wykresu scatter plot dla ilorazu przedmiotowa/ podmiotowa
+scatter_data = []
 
+for literature, values in test.items():
+    years = list(values.keys())
+    total_podmiotowa = [entry["literature"] for entry in values.values()]
+    total_przedmiotowa = [entry["secondary"] for entry in values.values()]
+    # total_combined = [p + s for p, s in zip(total_podmiotowa, total_przedmiotowa)]
+    total_combined = [s/p if p>0 else s for p, s in zip(total_podmiotowa, total_przedmiotowa)]
+    
+    scatter_data.extend(zip(years, total_combined, [literature] * len(years)))
 
+# Tworzenie DataFrame z danymi
+scatter_df = pd.DataFrame(scatter_data, columns=['Rok', 'Suma książek', 'Literatura'])
 
+# Posortuj DataFrame według roku (oś x) rosnąco
+scatter_df = scatter_df.sort_values(by='Rok')
 
+# Użycie innego palety kolorów
+palette = "Set1"  # Change this to the desired palette name
+
+# Tworzenie wykresu punktowo-liniowego (scatter plot) z wybraną paletą kolorów i posortowanymi danymi
+plt.figure(figsize=(10, 6), dpi=150)
+sns.set_palette(palette)  # Set the desired palette
+for literature in scatter_df['Literatura'].unique():
+    subset = scatter_df[scatter_df['Literatura'] == literature]
+    plt.scatter(subset['Rok'], subset['Suma książek'], label=literature, alpha=0.7)
+
+plt.title('Scatter plot – The quotient of secondary to literature books in different years')
+plt.xlabel('Year')
+plt.ylabel('Secondary book quotient')
+plt.legend()
+
+plt.xticks(rotation=90)
+
+plt.show()
 
 
 
