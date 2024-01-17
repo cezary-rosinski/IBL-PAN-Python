e624d336fe02ced32f81b354afc85d2544decd19
Cezary Rosiński <cezary.rosinski@gmail.com>
2024-01-17 15:44:02 +0100

    monita in another repository

--- monita_privata.py
+++
@@ -1,9 +0,0 @@
-import bibtexparser
-
-file = r"C:\Users\Cezary\Downloads\exportlist (1).txt"
-
-with open(file, encoding='utf-8') as bibtex_file:
-    bib_database = bibtexparser.load(bibtex_file)
-
-data = bib_database.entries
-print(bib_database.entries)
