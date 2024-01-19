c2584f27fd8c56b56f0747f2248c667a40df8055
Cezary Rosiński <cezary.rosinski@gmail.com>
2024-01-19 17:21:14 +0100

    iPBL statystyki poprawione

--- craft_oa_ojs_emails.py
+++ craft_oa_ojs_emails.py
@@ -13,44 +13,50 @@ import aiohttp
 import ssl
 
 #%%
-path = r'data\CRAFT-OA\beacon.csv'
-ojs_data = pd.read_csv(path)
+country_codes = pd.read_csv(r'data\CRAFT-OA\countries_codes_and_coordinates.csv')
+country_codes_dict = {k.replace('"', '').strip():v.replace('"', '').strip() for k,v in dict(zip(country_codes['Alpha-3 code'], country_codes['Alpha-2 code'])).items()}
+
+countries_ok = ["ALB", "AUT", "BIH", "BEL", "BGR", "CHE", "CYP", "CZE", "DEU", "DNK", "EST", "ESP", "FIN", "FRA", "GBR", "GRC", "HRV", "HUN", "IRL", "ISL", "ITA", "LTU", "LUX", "LVA", "MDA", "MNE", "MKD", "NLD", "NOR", "POL", "PRT", "ROU", "SRB", "SWE", "SRB", "SVK", "UKR"]
+countries_ok = [country_codes_dict.get(e) for e in countries_ok]
+
+ojs_countries = pd.read_csv(r'data\CRAFT-OA\beacon-summary.csv')
+ojs_data = pd.read_csv(r'data\CRAFT-OA\beacon.csv')
 
 ojs_data_selected = ojs_data[['oai_url', 'repository_name', 'country_consolidated']].drop_duplicates()
 ojs_data_selected = ojs_data_selected.loc[ojs_data_selected['repository_name'].notnull()]
+ojs_data_selected = ojs_data_selected.loc[ojs_data_selected['country_consolidated'].isin(countries_ok)]
 
-ojs_data_selected = ojs_data_selected.loc[ojs_data_selected['country_consolidated'] == 'PL']
+# ojs_data_selected = ojs_data_selected.loc[ojs_data_selected['country_consolidated'] == 'PL']
 
-oai_urls = set(ojs_data_selected['oai_url'].to_list())
+oai_urls = list(set(ojs_data_selected['oai_url'].to_list()))
 
-
-url_list = list(oai_urls)[:]
-
+n = 200
+url_lists = [oai_urls[i:i+n] for i in range(0,len(oai_urls),n)]
 
 async def fetch(session, url):
     async with session.get(url, ssl=ssl.SSLContext()) as response:
         return await response.text()
 
-
 async def fetch_all(urls, loop):
     async with aiohttp.ClientSession(loop=loop) as session:
         results = await asyncio.gather(*[fetch(session, url + '?verb=Identify') for url in urls], return_exceptions=True)
         return results
-
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    urls = url_list
-    htmls = loop.run_until_complete(fetch_all(urls, loop))
-    # print(htmls)
-
+    
 oai_harvesting = []
 pattern_email = "(?<=\<adminEmail\>)(.*)(?=\<\/adminEmail\>)"
 pattern_url = "(?<=\<baseURL\>)(.*)(?=\<\/baseURL\>)"
 pattern_name = "(?<=\<repositoryName\>)(.*)(?=\<\/repositoryName\>)"
-for e in htmls:
-    if e and not isinstance(e, aiohttp.client_exceptions.ClientConnectorError) and re.search(pattern_url, e):
-        oai_harvesting.append([re.search(pattern_url, e).group(), re.search(pattern_name, e).group(), re.search(pattern_email, e).group()])
+
+for url_list in tqdm(url_lists):
+    if __name__ == '__main__':
+        loop = asyncio.get_event_loop()
+        urls = url_list
+        htmls = loop.run_until_complete(fetch_all(urls, loop))
+        # print(htmls)
+    
+    for e in htmls:
+        if e and not isinstance(e, (aiohttp.client_exceptions.ClientConnectorError, aiohttp.client_exceptions.ClientPayloadError, aiohttp.client_exceptions.ClientOSError, TimeoutError, aiohttp.client_exceptions.ClientResponseError)) and re.search(pattern_url, e):
+            oai_harvesting.append([re.search(pattern_url, e).group(), re.search(pattern_name, e).group(), re.search(pattern_email, e).group()])
     
 df = pd.DataFrame(oai_harvesting, columns=['oai_url', 'repository_name', 'email']).drop_duplicates() 
    
