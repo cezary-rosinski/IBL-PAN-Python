920a3d1d8f20e070e8790bd875e5fe9413d04fed
Cezary Rosiński <cezary.rosinski@gmail.com>
2025-05-17 12:15:44 +0200

    MN opus

--- nowakowski_opus.py
+++ nowakowski_opus.py
@@ -9,6 +9,7 @@ df_people = gsheet_to_df('1ubUo9sdYQgH3xBpmi4FRDMRdTeuoEBbtrvmgHyH0wYg', 'Arkusz
 people_dict = dict(zip(df_people['Person:'].to_list(), df_people['Person Wikidata:'].to_list()))
 people_dict = {k:v for k,v in people_dict.items() if isinstance(v, str) and 'wikidata' in v}
 
+#
 key_authors = [[el.strip() for el in e.split(';')] if isinstance(e, str) else None for e in df_texts['Key authors cited:'].to_list()]
 
 key_authors_wikidata = []
@@ -23,7 +24,7 @@ test = pd.concat([df_texts[['Key authors cited:']], key_authors_df], axis=1)
 test['count_authors'] = test['Key authors cited:'].apply(lambda x: x.count(';') if isinstance(x, str) else 0)
 test['count_wiki'] = test[0].apply(lambda x: x.count(';') if isinstance(x, str) else 0)
 
-
+#
 response = [[el.strip() for el in e.split(';')] if isinstance(e, str) else None for e in df_texts['Response to author:'].to_list()]
 response_wikidata = []
 for i, authors in enumerate(response):
@@ -37,6 +38,20 @@ test = pd.concat([df_texts[['Response to author:']], response_df], axis=1)
 test['count_response'] = test['Response to author:'].apply(lambda x: x.count(';') if isinstance(x, str) else 0)
 test['count_wiki'] = test[0].apply(lambda x: x.count(';') if isinstance(x, str) else 0)
 
+#
+key_figures = [[el.strip() for el in e.split(';')] if isinstance(e, str) else None for e in df_texts['Key Historical Figures Mentioned'].to_list()]
+
+key_figures_wikidata = []
+for i, authors in enumerate(key_figures):
+    if authors:
+        key_figures_wikidata.append('; '.join([people_dict.get(e, 'no Wikidata ID') for e in authors]))
+    else: key_figures_wikidata.append(None)
+
+key_figures_df = pd.DataFrame(key_figures_wikidata)
+
+test = pd.concat([df_texts[['Key Historical Figures Mentioned']], key_figures_df], axis=1)
+test['count_response'] = test['Key Historical Figures Mentioned'].apply(lambda x: x.count(';') if isinstance(x, str) else 0)
+test['count_wiki'] = test[0].apply(lambda x: x.count(';') if isinstance(x, str) else 0)
 
 
 
