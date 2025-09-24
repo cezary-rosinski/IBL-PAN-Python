4283262e08b92dcab2b736b413e536b045f611a2
Cezary Rosiński <cezary.rosinski@gmail.com>
2025-09-24 14:28:32 +0200

    vienna start

---
+++ vienna_workshop_2025.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#%%
+
+df_artemis = pd.read_csv('data/vienna_workshop_2025/artemis_dataset_release_v0.csv')
+df_ola = pd.read_csv('data/vienna_workshop_2025/ola_dataset_release_v0.csv')
+
+
+#%%wikiart webscraping
+#https://www.wikiart.org/
+
+
+#%%wikidata enrichment
